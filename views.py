d65842ebfbc87c46d099aab04884944f763bb9c9
Maryam292 <132665838+Maryam292@users.noreply.github.com>
2024-03-20 14:06:00 +0500

    Create views.py

---
+++ views.py
@@ -0,0 +1,13 @@
+from .serializers import LogOutSerializer
+
+class LogOutAPIView(generics.GenericAPIView):
+  serializer_class = LogOutSerializer
+  permission_classes = (permissions.IsAuthenticated, )
+  def post(self, request):
+    serializer = self.serializer_class(data = request.data)
+    serializer.is_valid(raise_exception = True)
+    derializer.save()
+    return Responce(status = status.HTTP_204_NO_CONTENT)
+    
+  
+  
