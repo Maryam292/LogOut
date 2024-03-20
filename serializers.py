8cffa4cf8cd357057a2da2fde30d90ef53f4fd09
Maryam292 <132665838+Maryam292@users.noreply.github.com>
2024-03-20 14:30:22 +0500

    Updated serializers.py

--- serializers.py
+++ serializers.py
@@ -2,13 +2,18 @@ from rest_framework_simplejwt.tokens import RefreshToken, TokenError
 
 class LogOutSerializer(serializers.Serializer)
   refresh = serializers.CharField()
+  default_error_messages = {
+    'bad_token' : ('Token is expired or invalid')
+  }
+  
   def validate(self, attrs):
     self.token = attrs['refresh']
     return attrs
+    
   def save(self, **kwargs):
     try:
       RefreshToken(self.token).blacklist()
     except TokenError:
-      self.fail('bad token')
+      self.fail('bad_token')
     
     
